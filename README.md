98ce883f6c4da421265e2c8e4dbde5db81682753
Benoît <benoitobelia@gmail.com>
2023-11-20 10:21:37 +0100

    First commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# Memento
-Add post-it to your wall and remember your tasks
---
+++ index.php
@@ -0,0 +1,42 @@
+<?php 
+
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Memento</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Nanum+Pen+Script&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+<header>
+    <nav>
+        <ul>
+            <li><a href="">Memento</a></li>
+            <li><a href="login.php">Login</a></li>
+            <li><a href="register.php">Register</a></li>
+        </ul>
+    </nav>
+</header>
+
+
+<main>
+    <h1>Memento</h1>
+    <button>Ajouter une note</button>
+
+    <article class="">
+        <h3></h3>
+        <p></p>
+        <p></p>
+    </article>
+
+</main>
+
+</body>
+</html>
